9ebda7b1aae89690f576046029ecbe7ba6a6cc51
Doai Tran <josdoaitran@gmail.com>
2023-01-30 00:00:17 +0700

    update error handler

---
+++ student-svc/src/main/java/com/testing4everyone/studentsvc/config/ApiSubError.java
@@ -0,0 +1,19 @@
+package com.testing4everyone.studentsvc.config;
+
+public abstract class ApiSubError {
+}
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@AllArgsConstructor
+class ApiValidationError extends ApiSubError {
+    private String object;
+    private String field;
+    private Object rejectedValue;
+    private String message;
+
+    ApiValidationError(String object, String message) {
+        this.object = object;
+        this.message = message;
+    }
+}
--- student-svc/src/main/java/com/testing4everyone/studentsvc/model/Student.java
+++ student-svc/src/main/java/com/testing4everyone/studentsvc/model/Student.java
@@ -17,20 +17,17 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @NotEmpty @Min(3) @Max(10)
-    @Column(name = "name", unique = false)
+    @NotEmpty
     private String name;
     @Email(message = "Email is not valid", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
-    @NotEmpty(message = "Email cannot be empty")
     private String email;
 
     @NotNull
     private Gender gender;
-
-    @NotNull
+    @NotEmpty
     private Status status;
 
-    @NotEmpty @Min(5) @Max(50)
+    @NotEmpty
     private String address;
 
     @DateTimeFormat(pattern="MM/dd/yyyy")
